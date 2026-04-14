ec86eadd9d0a7faf1f1001b78a5eeb0284ddf4a9
Denis Vanyukov <cinets0@gmail.com>
2026-04-14 15:13:45 +0400

    Добавляет dateFormatter

--- components/cards/Card.tsx
+++ components/cards/Card.tsx
@@ -1,8 +1,10 @@
 import { CARD_TYPE_LABELS } from "@/constants/card"
 import type { CardItem } from "@/types/card"
 import styles from "./Card.module.scss"
+import { dateFormatter } from "@/lib/utils/date"
 
 export default function Card({ title, description, price, type, date }: CardItem) {
+
 
     return (
         <div className={styles.card}>
@@ -10,7 +12,7 @@ export default function Card({ title, description, price, type, date }: CardItem
                 <p className={styles.type}>
                     {CARD_TYPE_LABELS[type]}
                 </p>
-                <p className={styles.date}>{date}</p>
+                <p className={styles.date}>{dateFormatter(date)}</p>
             </div>
 
             <h3 className={styles.title}>{title}</h3>
